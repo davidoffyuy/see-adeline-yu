1a451f31b28956455c85655b7e3be2034c2946d2
David Yu <davidpyu@gmail.com>
2021-01-28 22:42:50 -0600

    Added image preloading

--- public/js/index.js
+++ public/js/index.js
@@ -1,13 +1,31 @@
 let gv = {};
 
 let adelineImages = [
-  "img01.jpg",
-  "img02.jpg",
-  "img03.jpg",
-  "img04.jpg",
-  "img05.jpg",
-  "img06.jpg"
+  "http://localhost:3000/adeline/img01.jpg",
+  "http://localhost:3000/adeline/img02.jpg",
+  "http://localhost:3000/adeline/img03.jpg",
+  "http://localhost:3000/adeline/img04.jpg",
+  "http://localhost:3000/adeline/img05.jpg",
+  "http://localhost:3000/adeline/img06.jpg"
 ];
+
+let images = [];
+let loadCount = 0;
+function addLoad() {
+  loadCount++;
+  console.log("loadCount: " + loadCount);
+  if (loadCount === images.length) {
+    document.getElementById("spinner-container").style.display = "none";
+  };
+}
+
+function preloadImages() {
+    for (var i = 0; i < arguments.length; i++) {
+        images[i] = new Image();
+        images[i].onload = addLoad;
+        images[i].src = preloadImages.arguments[i];
+    }
+}
 
 function resetView() {
   console.log("reset view");
@@ -28,7 +46,9 @@ const resetViewD = debouncer(() => resetView(), 300);
 // calls during DOM but
 window.addEventListener("DOMContentLoaded", (event) => {
   console.log("DOMContentLoaded event");
-  // load images
+  
+  // display loader
+  
 });
 
 window.addEventListener("load", (event) => {
@@ -41,9 +61,14 @@ window.addEventListener("load", (event) => {
   });
 
   // set the width/height of the image_container
-  document.getElementById("image-container").style.width = gv.bodyWidth + "px";
-  document.getElementById("image-container").style.height = gv.screenHeight + "px";
+  const imageContainer = document.getElementById("image-container");
+  imageContainer.style.width = gv.bodyWidth + "px";
+  imageContainer.style.height = gv.screenHeight + "px";
 
   // set div height equal to triple screen window height
   document.getElementById("double-blank").style.height = gv.screenHeight * 2 + "px";
+
+  // LAST - clear the loader
+  // document.getElementById("spinner-container").style.display = "none";
+  preloadImages(...adelineImages);
 });
